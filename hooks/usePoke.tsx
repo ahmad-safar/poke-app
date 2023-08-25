d0945f0b5226df3e7180e1544a0f3aa95edc7010
Ahmad Safar <ahmadsafar88@gmail.com>
2023-08-25 23:24:44 +0700

    fix: async fetch color with promise.all

--- hooks/usePoke.tsx
+++ hooks/usePoke.tsx
@@ -12,10 +12,17 @@ export default function usePoke() {
       "https://pokeapi.co/api/v2/pokemon?limit=20&offset=" + pageParam
     );
     const data = await res.json();
+    const colors = [];
     for (const key in data.results) {
       const url = new URL(data.results[key].url);
       const index = url.pathname.split("/")[4];
-      const color = await fetchColor(index);
+      colors[Number(index)] = fetchColor(index);
+      data.results[key].index = index;
+    }
+    const colorsData = await Promise.all(colors);
+
+    for (const key in data.results) {
+      const color = colorsData[data.results[key].index];
       let twColor
       switch (color) {
         case "white":
@@ -29,7 +36,6 @@ export default function usePoke() {
           break;
       }
       data.results[key].color = twColor;
-      data.results[key].index = index;
     }
     return data;
   };
