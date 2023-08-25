059e63bad8280fd61f469a601a85babc7e4d2186
Ahmad Safar <ahmadsafar88@gmail.com>
2023-08-25 22:45:54 +0700

    fix: remove unnecessary Promise

--- hooks/usePoke.tsx
+++ hooks/usePoke.tsx
@@ -1,10 +1,10 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 const fetchColor = async (index: string) => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${index}`);
-    const data = await res.json();
-    return data.color.name;
-    };
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${index}`);
+  const data = await res.json();
+  return data.color.name;
+};
 
 export default function usePoke() {
   const fetchPoke = async ({ pageParam = "0" }) => {
@@ -12,16 +12,13 @@ export default function usePoke() {
       "https://pokeapi.co/api/v2/pokemon?limit=20&offset=" + pageParam
     );
     const data = await res.json();
-    await new Promise(async (resolve) => {
-      for (const key in data.results) {
-        const url = new URL(data.results[key].url);
-        const index = url.pathname.split("/")[4];
-        const color = await fetchColor(index);
-        data.results[key].color = color;
-        data.results[key].index = index;
-      }
-      resolve(data);
-    });
+    for (const key in data.results) {
+      const url = new URL(data.results[key].url);
+      const index = url.pathname.split("/")[4];
+      const color = await fetchColor(index);
+      data.results[key].color = color;
+      data.results[key].index = index;
+    }
     return data;
   };
 
